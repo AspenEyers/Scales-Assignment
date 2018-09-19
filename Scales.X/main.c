9c66bfdb75dbdf5d58f364f9800d54557a634cbb
Aspen <aspen.jje@gmail.com>
2018-09-20 06:27:11 +1000

    adding doxygen

--- Scales.X/main.c
+++ Scales.X/main.c
@@ -1,19 +1,32 @@
+/****************************************************************************
+*                                                                          *
+* This file is the main fie for the scales assignemnt.                     *
+****************************************************************************/
+
+
 /*
- * File:   main.c
- * Authors:
- * Aspen, James, Tom, Lina, Kwajo, Ty, Richard
+ * @File:   main.c
+ * @Authors: Aspen, James, Tom, Lina, Kwajo, Ty, Richard
+ * @date 18 September 2018
+ * @ Brief I guess we need to 
  *
- * Created on 18 September 2018, 11:01 AM
  */
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
+
+
+
 
 /*
  *
  */
 void main(void) {
 
-  testkjlbdfjk
+  int state = USER_LOCAL;
+
+
 
 }
