5dc45e9d37b6c272bb15c7db3c1ed5dbca385fcd
Aspen <aspen.jje@gmail.com>
2018-10-20 12:30:31 +1100

    AD working
    and num2string function was made

--- seven_b_4520.X/main.c
+++ seven_b_4520.X/main.c
@@ -42,6 +42,7 @@ int direction;
 void highPriorityISR( void );
 void lowPriorityISR( void );
 void AdInit(void);
+void num2str(char *string, int number);
 
 // Setup the interrupts 
 #pragma code highISR = 0x08
@@ -90,128 +91,36 @@ void main( void )
 
     
     // ******** Verify the LCD and serial work ******** //
+    //num2str(&output,987);
     lcd_clear();    
     write_string(0,0,message);
     tx232C(intro_msg);
     tx232C(end_msg);
+    //while(1);
+    AdInit();   
     
- 
+
+    while(1){
+        // poll the A/D
+        while(ADCON0bits.GO == 1 ){
+            // Do nothing
+        }
+
+        number = ADRESH;
+        ADCON0bits.GO = 1;
+
+        num2str(&output,number);
+        write_string(0,0,output);
+    }
     
+    num2str(&output,987);
+    
+    //TRISAbits.RA0 = 1;
     number = 1;
     direction = 1;
-    while(1){
-    
-    length = 0;    
-    rem = number;
-    
-    // ********                     ******** //
-    // Determine the length of the input number
-    while(rem > 0){
-        length++;
-        rem = rem/10;
-    }
-    
-    output[0] = (length+48);
-    output[1] = '\0';
-    
-    // ********                      ******** //
-    // Convert the number into a char array
-    rem = number;
-    i = length;
-    
-    while(length > 0){
-        
-        output[--length] = (rem%10 + 48);
-        rem = rem/10;
-        //length--;
-    }
-    output[i++] = ',';
-    output[i++] = '\n';
-    output[i++] = '\r';
-    output[i++] = '\0';
-            
     
     
-    // *********** Display the data *********** //
-    tx232C(output);
-    write_string(0,0,output);
-    
-    
-    // ******** Simulate data changing ******** //
-    number = number + direction;
-    if(number > 1000){
-        
-        direction = -1;
-        
-    }else if(number <= 0){
-        direction = 1;
-    }
-    }    
-    // Number has been converted and sent
-    
-    
-    
-    /*
-    
-    while(1){
-        // If there was a message received by the serial communications
-        if(MsgToSend){
-            //lcd_clear();
-            //write_string(0,0,fromReceiver);
-            tx232C(fromReceiver);
-            tx232C(end_msg);
-            MsgToSend = 0;
-            switch(state){
-                case SET_ROW:
-                    //tx232C(debug);
-                    if(strcmp(fromReceiver, num_arr[1]) == 0){
-                        row = 1;
-                        //tx232C(fromReceiver);
-                        //tx232C(end_msg);
-                        state++;
-                    }else if(strcmp(fromReceiver, num_arr[0]) == 0){
-                        row = 0;
-                        //tx232C(fromReceiver);
-                        //tx232C(end_msg);
-                        state++;
-                    }
-                    break;
-                case SET_POS:
-                    
-                    // for each string, if equal then set pos and move on
-                    for(i=0; i < BUFFERSIZE; i++){
-                        if(strcmp(fromReceiver, num_arr[i])==0){
-                            position = i;
-                            //tx232C(fromReceiver);
-                            //tx232C(end_msg);
-                            state++;
-                        }
-                    }
-                    
-                    
-                    
-                    break;
-                case SEND_MSG:
-                    
-                    //tx232C(fromReceiver);
-                    //tx232C(end_msg);
-                    lcd_clear();
-                    write_string(row,position,fromReceiver);
-                    state = SET_ROW;
-                    
-                    
-                    break;
-                    
-                default:
-                    tx232C(debug);
-                    
-            }
-        }
-    }
-    */
 }
-
-
 
 
 
@@ -236,20 +145,17 @@ void lowPriorityISR( void ){
 } 
 
 void AdInit(void){
-    
-    ADCON0bits.ADON = 1;    // Enable A/D
-    // ADCON0bits.GO_DONE   // AD state, 1 if in progress   
-    
-    // Choose channel AN0
-    ADCON0bits.CHS0 = 0;
+
+    // Choose channel AN1
+    ADCON0bits.CHS0 = 1;
     ADCON0bits.CHS1 = 0;
     ADCON0bits.CHS2 = 0;
     ADCON0bits.CHS3 = 0;
     
     // Set all the port A pins to Analog <<<<<<<<<<<<<<<<<<<<<<
-    ADCON1bits.PCFG0 = 0;
-    ADCON1bits.PCFG1 = 0; 
-    ADCON1bits.PCFG2 = 0;
+    ADCON1bits.PCFG0 = 1;
+    ADCON1bits.PCFG1 = 1; 
+    ADCON1bits.PCFG2 = 1;
     ADCON1bits.PCFG3 = 0;
             
     // Voltage reference config bits
@@ -257,16 +163,67 @@ void AdInit(void){
     ADCON1bits.VCFG0 = 0;   // 1 = Vref- reference is AN2
     ADCON1bits.VCFG1 = 0;   // 1 = Vref+ reference is AN3 
 
-    // FOSC/8 clock
+    // RC oscilator
     ADCON2bits.ADCS0 = 1;
-    ADCON2bits.ADCS1 = 0;
-    ADCON2bits.ADCS2 = 0;
+    ADCON2bits.ADCS1 = 1;
+    ADCON2bits.ADCS2 = 1;
 
-    // Aquisition time
+    // Aquisition time = 8 T_AD
+    // Section 19.1 explains why this is nessiscary
     ADCON2bits.ACQT0 = 0;
     ADCON2bits.ACQT1 = 0;
-    ADCON2bits.ACQT2 = 0;
+    ADCON2bits.ACQT2 = 1;
     // Left justify
     ADCON2bits.ADFM = 0;         
+    
+        ADCON0bits.ADON = 1;    // Enable A/D
+    // ADCON0bits.GO_DONE   // AD state, 1 if in progress   
+        
+        Nop();
+        Nop();
+        Nop();
+        Nop();
+        Nop();
+        
+        // start the conversion
+        // this needs to be done every time
+        ADCON0bits.GO = 1;
 
+}
+
+
+
+// This function takes in a pointer to a string and
+// a number. The number will be saved to that string
+// The function returns nothing.
+// The function tests each character of the number and 
+// will return when it reaches 0. therefore a number starting
+// with a 0 e.g. 098. will write nothing to the string.
+void num2str(char *string, int number){
+    
+    int length = 0;    
+    int rem = number;
+    int i = 0;
+    
+    // ********                     ******** //
+    // Determine the length of the input number
+    while(rem > 0){
+        length++;
+        rem = rem/10;
+    }
+    
+    // ********                      ******** //
+    // Convert the number into a char array
+    rem = number;
+    i = length;
+    
+    while(length > 0){
+        
+        *(string+length-1) = (rem%10 + 48);
+        length--;
+        rem = rem/10;
+    }
+    *(string+i) = ',';
+    i++;
+    *(string+i) = '\0';
 }
